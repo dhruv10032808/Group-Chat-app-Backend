68c48c0f5bae1aa8620c94b64b7b61958a15ebc3
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-04-02 14:11:58 +0530

    signup backend

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const cors=require('cors');
 const userRoutes=require('./routes/user')
 const sequelize=require('./util/database')
 const app=express();
-app.use(cors());
+app.use(cors({
+    origin:'http://192.168.0.103:5500'
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(userRoutes)
--- controllers/user.js
+++ controllers/user.js
@@ -2,11 +2,6 @@ const User=require('../models/user');
 const bcrypt=require('bcrypt')
 
 exports.signup=(req,res,next)=>{
-    User.findAll({where:{email:req.body.email}}).then(users=>{
-        if(users.length>0){
-          return res.status(400).json({message:'User already exists'})
-        }
-        else{
             bcrypt.hash(req.body.password,10,(err,hash)=>{
                 if(err){
                     console.log('something went wrong')
@@ -23,6 +18,4 @@ exports.signup=(req,res,next)=>{
                     res.json({newUserDetail:err})
                     console.log(err)})
             })
-        }
-    })
-}
+    }
